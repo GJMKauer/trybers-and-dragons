b2b2de7d7eadbbdb083fdb2a3d01f7c3fb915410
Gabriel Kauer <gjmkauer@hotmail.com>
2022-09-22 15:48:46 -0300

    FEAT: add req5 - create Mage, Necromancer, Ranger and Warrior archetypes and implementations.

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,16 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Mage extends Archetype {
+  private static _archetypeInstances = 0;
+  private _energyType: EnergyType = 'mana';
+
+  public static createdArchetypeInstances(): number {
+    this._archetypeInstances += 1;
+    return this._archetypeInstances;
+  }
+
+  public get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,3 +1,8 @@
 import Archetype from './Archetype';
+import Mage from './Mage';
+import Necromancer from './Necromancer';
+import Warrior from './Warrior';
+import Ranger from './Ranger';
 
 export default Archetype;
+export { Mage, Necromancer, Warrior, Ranger };
